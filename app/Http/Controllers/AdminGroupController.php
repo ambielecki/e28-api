9fa48d219ea7b8331f406ffba1291c0e051453a2
Andrew Bielecki <ambielecki@gmail.com>
2020-02-05 18:06:11 -0500

    group create

--- app/Http/Controllers/AdminGroupController.php
+++ app/Http/Controllers/AdminGroupController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Group;
 use App\Http\Requests\GroupRequest;
+use App\User;
+use Hash;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Session;
+use Str;
 
 class AdminGroupController extends Controller
 {
@@ -38,7 +42,36 @@ class AdminGroupController extends Controller
         $group->name = $request->input('name');
         $group->options = [];
 
-        // TODO: Save and attach users
+        if ($group->save()) {
+            $user_emails = $request->input('users');
+            $group_users = User::query()
+                ->whereIn('email', $request->input('users'))
+                ->pluck('email', 'id')
+                ->toArray();
+
+            $new_emails = array_diff($user_emails, $group_users);
+
+            foreach ($new_emails as $new_email) {
+                $user = new User();
+                $user->first_name = '';
+                $user->last_name = '';
+                $user->email = $new_email;
+                $user->password = Hash::make(Str::random(8));
+                $user->is_admin = false;
+
+                $user->save();
+
+                $group_users[$user->id] = $user->email;
+            }
+
+            $group->users()->attach(array_keys($group_users));
+
+            Session::flash('flash_success', "Group $group->name Created Successfully");
+
+            return redirect()->route('group_list');
+        }
+
+        Session::flash('flash_warning', 'There was a problem saving your group, please try again');
 
         return back()->withInput();
     }
--- app/Http/Requests/GroupRequest.php
+++ app/Http/Requests/GroupRequest.php
@@ -24,7 +24,8 @@ class GroupRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'users.*' => 'email'
         ];
     }
 }
