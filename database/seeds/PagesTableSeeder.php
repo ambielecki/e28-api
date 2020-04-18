3ea5cf001e8f7295044e329e73ae04f11c9a3ed1
Andrew Bielecki <ambielecki@gmail.com>
2020-04-18 09:03:29 -0400

    seed with initial homepage

--- database/seeds/PagesTableSeeder.php
+++ database/seeds/PagesTableSeeder.php
@@ -16,28 +16,11 @@ class PagesTableSeeder extends Seeder
         $page->app = Page::APP_BEER;
         $page->page = Page::PAGE_TYPE_HOME;
         $page->content = <<<TEXT
-<p>Homebrew Helper is a simple proof of concept application that allows a users to save and rate 
-their homebrew recipes.  The journal tab will show a list of previous brews, with some of their 
-vital stats, so you can simply find your favorites and recreate them (or just reminisce a little);
-</p>
-<p>
-One can also find a work in progress collection of helpful tools under, you guessed it, the tools 
-link. Currently limited to just an ABV calculator, but more will be added (time and interest 
-permitting).
-</p>
-<p>
-I am a novice at the homebrew game and felt this would be the perfect way to help me track my journey. 
-Surely there are fields missing, and the filtering and searching is definitely primitive at the moment. 
-But now is definitely the time for time for hobbies, whether it is making sure you are supplied with 
-homebrew.
-</p>
-<p>
-On the technical side, this site is also an experiment (for class), written as a full Single Page 
-Application using Vue.js.  The site is backed by a Laravel API and hosted on Digital Ocean.
-</p>
-<p>
-Whether it is coding or homebrew, now is a great time to start and track a new hobby!
-</p>
+<p><strong>Homebrew Helper</strong> is a simple proof of concept application that allows a users to save and rate their homebrew recipes. The journal tab will show a list of previous brews, with some of their vital stats, so you can simply find your favorites and recreate them (or just reminisce a little);</p>
+<p>One can also find a work in progress collection of helpful tools under, you guessed it, the tools link. Currently limited to just an ABV calculator, but more will be added (time and interest permitting).</p>
+<p>I am a novice at the homebrew game and felt this would be the perfect way to help me track my journey. Surely there are fields missing, and the filtering and searching is definitely primitive at the moment. But now is definitely the time for time for hobbies, whether it is making sure you are supplied with homebrew.</p>
+<p>On the technical side, this site is also an experiment (for class, <a href="https://hesweb.dev/e28">DGMD E-28 at Harvard Extension</a>), written as a full Single Page Application using Vue.js. The site is backed by a Laravel API and hosted on Digital Ocean.</p>
+<p>Whether it is coding or homebrew, now is a great time to start and track a new hobby!</p>
 TEXT;
         $page->save();
     }
